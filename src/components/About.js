cb9bcf8467847a8e707fd8326ba484e8ae503ba6
emre <emre27817@gmail.com>
2023-03-17 14:29:51 +0300

    Burger Projesi

---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import BannerImage from '../assets/banneryeni.jpg'
+import '../styles/About.css'
+
+function About() {
+  return (
+    <div className='about'>
+        <div className='aboutTop' style={{backgroundImage:`url(${BannerImage})`}}></div>
+        <div className='aboutBottom'>
+            <h1>Hakkımızda</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, id sed! Nesciunt eius explicabo ipsum omnis aliquam! Eius iure dicta cupiditate rerum ad fuga accusantium incidunt eos reiciendis, tenetur deserunt aliquam exercitationem esse libero excepturi sunt numquam ullam animi ab ex aspernatur omnis veniam vitae? Sit enim ipsa a et vel dolores eligendi laboriosam sequi minus nostrum possimus tempore, nam dicta esse animi beatae, laborum cumque praesentium accusantium, maiores sed asperiores cum nobis velit? Eligendi aliquid neque cupiditate libero ducimus similique consectetur earum perspiciatis accusamus velit, voluptate saepe nulla obcaecati nobis consequuntur alias repudiandae incidunt. Id, magnam itaque omnis eaque corporis officia vitae minima temporibus. Repellendus eveniet earum, minus alias officia nihil id quis! Ea recusandae sunt, maxime rem tempore minima ullam cumque, esse nobis perspiciatis facilis? Saepe dicta magni quis totam unde! Minus eaque veniam distinctio tenetur officia quas ullam praesentium quia, nesciunt laudantium voluptatem quidem recusandae laborum obcaecati, consequuntur quae exercitationem eum suscipit a illum? Maxime fuga dicta est commodi aut sint sunt nulla, sapiente rerum autem quisquam dolore blanditiis magnam voluptatum corporis cum repellat porro, aliquam quas voluptas harum? Porro, optio, alias consequuntur eius dolor obcaecati labore atque assumenda rerum libero facilis tenetur dolorem quis neque voluptatum?</p>
+        </div>
+    </div>
+  )
+}
+
+export default About
+
